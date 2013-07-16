19e0103f9d4ec599c2477f68c4f5baea40c9fece
akima <akm2000@gmail.com>
2013-07-16 20:53:20 +0900

    add bin/skygrepe with config file

--- lib/skygrepe.rb
+++ lib/skygrepe.rb
@@ -3,16 +3,14 @@ require "sqlite3"
 
 module Skygrepe
 
-  class << self
-    def database
-      user = ENV['SKYPE_USER']
-      raise "$SKYPE_USER is required" unless user
-      @database ||= SQLite3::Database.new("#{ENV['HOME']}/Library/Application\ Support/Skype/#{user}/main.db")
+  class Database
+    def initialize(path)
+      @impl ||= SQLite3::Database.new(path)
     end
 
     def grep(keyword)
       sql = "SELECT m.timestamp, c.displayname, m.author, m.body_xml FROM Messages as m inner join Conversations as c on m.convo_id = c.id  WHERE body_xml like '%#{keyword}%';"
-      rows = database.execute(sql)
+      rows = @impl.execute(sql)
     end
   end
 end
