0f70db45ecbdb61dccb96d47e0d4cb29ce7572ae
akima <akm2000@gmail.com>
2013-07-16 20:36:52 +0900

    最低限の接続確認できました

--- lib/skygrepe.rb
+++ lib/skygrepe.rb
@@ -1,4 +1,18 @@
 require "skygrepe/version"
+require "sqlite3"
 
 module Skygrepe
+
+  class << self
+    def database
+      user = ENV['SKYPE_USER']
+      raise "$SKYPE_USER is required" unless user
+      @database ||= SQLite3::Database.new("#{ENV['HOME']}/Library/Application\ Support/Skype/#{user}/main.db")
+    end
+
+    def grep(keyword)
+      sql = "SELECT m.timestamp, c.displayname, m.author, m.body_xml FROM Messages as m inner join Conversations as c on m.convo_id = c.id  WHERE body_xml like '%#{keyword}%';"
+      rows = database.execute(sql)
+    end
+  end
 end
